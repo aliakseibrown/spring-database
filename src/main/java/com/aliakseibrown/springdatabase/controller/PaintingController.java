43f319d73586fdb76de4fa5de5a33945c272f226
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-01-11 23:20:23 +0100

    configuration of @RestController to return jsp page

--- src/main/java/com/aliakseibrown/springdatabase/controller/PaintingController.java
+++ src/main/java/com/aliakseibrown/springdatabase/controller/PaintingController.java
@@ -4,23 +4,22 @@ import com.aliakseibrown.springdatabase.dao.PaintingRepo;
 import com.aliakseibrown.springdatabase.model.Painting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class PaintingController {
     @Autowired
     PaintingRepo repo;
 
     @RequestMapping("/")
-    public String home(){
-        return "home.jsp";
+    public ModelAndView home(){
+        ModelAndView mav = new ModelAndView( "home.jsp");
+        return mav;
+        // @RestController doesn't return jsp files, works only with @Controller
     }
     @RequestMapping("/addPainting")
     public String addPainting(Painting painting){
@@ -28,18 +27,24 @@ public class PaintingController {
         return "home.jsp";
     }
 
-    @RequestMapping("/paintings")
-    @ResponseBody
+    @RequestMapping(path="/paintings", produces={"application/json"}) //set GET receive only xml format of data
+    //@RequestMapping("/paintings")
     public List<Painting> getPaintings(){
-        return repo.findAll(); //json format;
+        return repo.findAll();
     }
-    @RequestMapping("/painting/{id}")
-    @ResponseBody
+
+    @PostMapping(path="/painting",produces={"application/json"})
+    public Painting getPaintings(Painting painting){
+        repo.save(painting);
+        return painting;
+    }
+
+    @RequestMapping(path="/painting/{id}", produces={"application/json"})
     public Optional<Painting> getPainting(@PathVariable("id") int id){
         return repo.findById(id);
     }
 
-    @RequestMapping("/getPainting")
+    @RequestMapping(path="/getPainting",produces={"application/json"})
     public ModelAndView getPainting(@RequestParam Integer id){
         ModelAndView mv = new ModelAndView("showPainting.jsp");
         Painting painting = repo.findById(id).orElse(new Painting());
